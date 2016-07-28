280591bfb5cb0a3ab88e42ee7ba0eb001abc0673
Casey Cross <hellocaseycross@gmail.com>
2016-07-28 15:58:27 -0600

    Refactor food corner tests.

--- test/food-test.js
+++ test/food-test.js
@@ -34,138 +34,58 @@ describe('Food', function(){
 
 describe('topLeft()', function () {
 
-    it('should have a method called "topLeft()"', function () {
-      var food = new Food(100, 50, 10, 10);
-      assert.isFunction(food.topLeft);
-    });
-
-    it('should return an object', function () {
+    it('should have a method called topLeft() that returns an object', function () {
       var food = new Food(100, 50, 10, 10);
       assert.isObject(food.topLeft());
     });
 
-    it('should return an object with an "x" property that is a number', function () {
-      var food = new Food(100, 50, 10, 10);
-      assert.isNumber(food.topLeft().x);
-    });
-
-    it('should return an object with a "y" property that is a number', function () {
-      var food = new Food(100, 50, 10, 10);
-      assert.isNumber(food.topLeft().y);
-    });
-
-    it('should return the x coordinate of the topLeft corner of the food', function () {
+    it('should return the x and y coordinates of the topLeft corner of the food', function () {
       var food = new Food(100, 50, 10, 10);
       assert.equal(food.topLeft().x, food.x);
-    });
-
-    it('should return the y coordinate of the topLeft corner of the food', function () {
-      var food = new Food(100, 50, 10, 10);
       assert.equal(food.topLeft().y, food.y);
     });
-
   });
 
   describe('topRight()', function () {
 
-    it('should have a method called "topRight()"', function () {
-      var food = new Food(100, 50, 10, 10);
-      assert.isFunction(food.topRight);
-    });
-
-    it('should return an object', function () {
+    it('should have a method called topRight() that returns an object', function () {
       var food = new Food(100, 50, 10, 10);
       assert.isObject(food.topRight());
     });
 
-    it('should return an object with an "x" property that is a number', function () {
-      var food = new Food(100, 50, 10, 10);
-      assert.isNumber(food.topRight().x);
-    });
-
-    it('should return an object with a "y" property that is a number', function () {
-      var food = new Food(100, 50, 10, 10);
-      assert.isNumber(food.topRight().y);
-    });
-
-    it('should return the x coordinate of the topRight corner of the food', function () {
+    it('should return the x and y coordinates of the topRight corner of the food', function () {
       var food = new Food(100, 50, 10, 10);
       assert.equal(food.topRight().x, food.x + food.width);
-    });
-
-    it('should return the y coordinate of the topRight corner of the food', function () {
-      var food = new Food(100, 50, 10, 10);
       assert.equal(food.topRight().y, food.y);
     });
-
   });
 
   describe('bottomLeft()', function () {
 
-    it('should have a method called "bottomLeft()"', function () {
-      var food = new Food(100, 50, 10, 10);
-      assert.isFunction(food.bottomLeft);
-    });
-
-    it('should return an object', function () {
+    it('should have a method called bottomLeft() that returns an object', function () {
       var food = new Food(100, 50, 10, 10);
       assert.isObject(food.bottomLeft());
     });
 
-    it('should return an object with an "x" property that is a number', function () {
-      var food = new Food(100, 50, 10, 10);
-      assert.isNumber(food.bottomLeft().x);
-    });
-
-    it('should return an object with a "y" property that is a number', function () {
-      var food = new Food(100, 50, 10, 10);
-      assert.isNumber(food.bottomLeft().y);
-    });
-
-    it('should return the x coordinate of the bottomLeft corner of the food', function () {
+    it('should return the x and y coordinates of the bottomLeft corner of the food', function () {
       var food = new Food(100, 50, 10, 10);
       assert.equal(food.bottomLeft().x, food.x);
-    });
-
-    it('should return the y coordinate of the bottomLeft corner of the food', function () {
-      var food = new Food(100, 50, 10, 10);
       assert.equal(food.bottomLeft().y, food.y + food.height);
     });
-
   });
 
   describe('bottomRight()', function () {
 
-    it('should have a method called "bottomRight()"', function () {
-      var food = new Food(100, 50, 10, 10);
-      assert.isFunction(food.bottomRight);
-    });
-
-    it('should return an object', function () {
+    it('should have a method called bottomRight() that returns an object', function () {
       var food = new Food(100, 50, 10, 10);
       assert.isObject(food.bottomRight());
     });
 
-    it('should return an object with an "x" property that is a number', function () {
-      var food = new Food(100, 50, 10, 10);
-      assert.isNumber(food.bottomRight().x);
-    });
-
-    it('should return an object with a "y" property that is a number', function () {
-      var food = new Food(100, 50, 10, 10);
-      assert.isNumber(food.bottomRight().y);
-    });
-
-    it('should return the x coordinate of the bottomRight corner of the food', function () {
+    it('should return the x and y coordinates of the bottomRight corner of the food', function () {
       var food = new Food(100, 50, 10, 10);
       assert.equal(food.bottomRight().x, food.x + food.width);
-    });
-
-    it('should return the y coordinate of the bottomRight corner of the food', function () {
-      var food = new Food(100, 50, 10, 10);
       assert.equal(food.bottomRight().y, food.y + food.height);
     });
-
   });
 
 });
