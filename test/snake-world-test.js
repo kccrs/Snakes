87a046b89725eb1e2896ba46370cbb9cb517c1e1
Casey Cross <hellocaseycross@gmail.com>
2016-08-01 10:46:40 -0600

    Clean up old tests that no longer apply.

--- test/snake-world-test.js
+++ test/snake-world-test.js
@@ -7,44 +7,23 @@ const World = require('../lib/world');
 describe('Snake in relation to the World', function() {
 
   it('should know about the World if it is passed in', function() {
-    var world = new World (100, 100);
-    var snake = new Snake (20, 20, 20, 20, world);
-    assert.equal(snake.world, world);
-  });
-
-  it('should know about a world if it is created using world.addSnake', function() {
-    var world = new World (100, 100);
-    var snake = new Snake (20, 20, 20, 20, world);
-    world.addSnake(snake);
+    var world = new World (400, 300);
+    var snake = new Snake (0, 0, 20, 20, world);
     assert.equal(snake.world, world);
   });
 });
 
 describe('World in relation to snake', function() {
 
-  it('should have a method called addSnake()', function(){
-      var world = new World(100, 100);
-      assert.isFunction(world.addSnake);
-    });
-
-    it('should know about the snakes added to the world', function() {
-      var world = new World(100, 100);
-      var snake = new Snake(0, 0, 20, 20);
-      world.addSnake(snake);
-      assert.include(world.snakes, snake);
-    });
-
-
   it('should have a rightArrow() method', function() {
     var world = new World(100, 100);
     assert.isFunction(world.rightArrow);
   });
 
-  it('should move the snake right when the rightArrow() is pressed', function() {
+  it('should move the snake right when the moveRight() is pressed', function() {
     var world = new World (100, 100);
     var snake = new Snake (20, 20, 20, 20);
-    world.addSnake(snake);
-    world.rightArrow();
+    snake.moveRight();
     assert.equal(snake.x, 40);
   });
 
@@ -53,11 +32,10 @@ describe('World in relation to snake', function() {
     assert.isFunction(world.leftArrow);
   });
 
-  it('should move the snake left when the leftArrow() is pressed', function() {
+  it('should move the snake left when the moveLeft() is pressed', function() {
     var world = new World (100, 100);
     var snake = new Snake (20, 20, 20, 20);
-    world.addSnake(snake);
-    world.leftArrow();
+    snake.moveLeft();
     assert.equal(snake.x, 0);
   });
 
@@ -66,11 +44,10 @@ describe('World in relation to snake', function() {
     assert.isFunction(world.upArrow);
   });
 
-  it('should move the snake up when the upArrow() is pressed', function() {
+  it('should move the snake up when moveUp() is called', function() {
     var world = new World (100, 100);
     var snake = new Snake (20, 20, 20, 20);
-    world.addSnake(snake);
-    world.upArrow();
+    snake.moveUp();
     assert.equal(snake.y, 0);
   });
 
@@ -79,11 +56,10 @@ describe('World in relation to snake', function() {
     assert.isFunction(world.downArrow);
   });
 
-  it('should move the snake down when the downArrow() is pressed', function() {
+  it('should move the snake down when moveDown() is called', function() {
     var world = new World (100, 100);
     var snake = new Snake (20, 20, 20, 20);
-    world.addSnake(snake);
-    world.downArrow();
+    snake.moveDown();
     assert.equal(snake.y, 40);
   });
 
@@ -91,7 +67,7 @@ describe('World in relation to snake', function() {
 
     it('should have a method called isSnakeEating()', function() {
       var world = new World(100, 100);
-      
+
       assert.isFunction(world.isSnakeEating);
     });
 
